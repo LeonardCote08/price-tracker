715c2f81a4a4dd1fffcb082c7d2555f064936eec
Léonard <leonard.cote08@gmail.com>
2025-02-19 20:47:42 -0500

    Correction mineure

--- scrapers/spiders/ebay_spider.py
+++ scrapers/spiders/ebay_spider.py
@@ -31,8 +31,7 @@ class EbaySpider(scrapy.Spider):
         "AUTOTHROTTLE_ENABLED": True,
         "AUTOTHROTTLE_START_DELAY": 2.0,
         "AUTOTHROTTLE_MAX_DELAY": 10.0,
-        "AUTOTHROTTLE_TARGET_CONCURRENCY": 2.0,
-        "CLOSESPIDER_PAGECOUNT" : 5
+        "AUTOTHROTTLE_TARGET_CONCURRENCY": 2.0
 
     }
 
