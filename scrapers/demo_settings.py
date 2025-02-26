e499cc13d4632a415d1b4f22b03b9c7b26926d5c
Léonard <leonard.cote08@gmail.com>
2025-02-25 21:06:40 -0500

    dasd

--- scrapers/demo_settings.py
+++ scrapers/demo_settings.py
@@ -5,5 +5,9 @@ from scrapers.settings import *
 # Enable demo mode
 DEMO_MODE = True
 
+TELNETCONSOLE_ENABLED = False
+
+LOG_FORMAT = '%(levelname)s: %(message)s'
+
 # Set log level to INFO to show essential messages
 LOG_LEVEL = "INFO"
